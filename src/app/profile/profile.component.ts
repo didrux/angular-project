115ac392f0b19c2c9273be89eb1365ea41ead896
Michael Ovdiienko <mishaov@gmail.com>
2022-11-23 01:18:27 +0200

    feat: add profile form

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css'],
 })
-export class ProfileComponent {}
+export class ProfileComponent {
+
+  constructor(private formBuilder: FormBuilder, private router: Router) {
+  }
+
+  profileLogout = this.formBuilder.group({
+  });
+
+  logout(): void {
+    localStorage.clear();
+    console.log("kek");
+    this.router.navigate(['/news']);
+  }
+
+}
+
